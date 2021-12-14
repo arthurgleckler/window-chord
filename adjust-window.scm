cf84ef57ccbbb07508c9fb80529cef23187bc0e9
Arthur A. Gleckler <aag@alum.mit.edu>
2021-12-13 17:57:56 -0800

    initial checkin

---
+++ adjust-window.scm
@@ -0,0 +1,92 @@
+;; <> Get these from the root window.
+(define (root-height) 1080)
+(define (root-width) 1920)
+
+(define-record-type extents
+    (make-extents left right top bottom)
+    extents?
+  (bottom xt/bottom)
+  (left xt/left)
+  (right xt/right)
+  (top xt/top))
+
+(define (active-window)
+  (process->string '("xdotool" "getactivewindow")))
+
+(define (xprop name window)
+  (process->string `("xprop" "-id" ,window ,name)))
+
+(define (xprop-extents name window transform)
+  (let* ((output (xprop name window))
+	 (=-cursor (string-index output (lambda (c) (char=? c #\=)))))
+    (and (not (string-cursor=? =-cursor (string-cursor-end output)))
+	 (apply make-extents
+		(map (lambda (s) (transform (string->number (string-trim s))))
+		     (string-split
+		      (string-copy/cursors
+		       output
+		       (string-cursor-forward output =-cursor 1))
+		      ","))))))
+
+(define (frame-extents-gtk window)
+  (xprop-extents "_GTK_FRAME_EXTENTS" window +))
+
+(define (frame-extents-net window)
+  (xprop-extents "_NET_FRAME_EXTENTS" window -))
+
+(define (system-for-effect command arguments)
+  (or (apply system? command arguments)
+      (error "Call failed." command)))
+
+(define (xdotool . arguments)
+  (define (stringify x)
+    (cond ((number? x) (number->string x))
+	  ((string? x) x)
+	  ((symbol? x) (symbol->string x))
+	  (else (error "Unexpected type." x))))
+  (system-for-effect "xdotool"
+		     (map stringify arguments)))
+
+(define (window-extents window)
+  (or (frame-extents-gtk window)
+      (frame-extents-net window)
+      (make-extents 0 0 0 0)))
+
+(define (half-screen extents window)
+  (xdotool 'windowsize
+	   window
+	   (+ (/ (root-width) 2)
+	      (xt/left extents)
+	      (xt/right extents))
+	   (+ (root-height)
+	      (xt/top extents)
+	      (xt/bottom extents))))
+
+(define (left-half window)
+  (let ((extents (window-extents window)))
+    (half-screen extents window)
+    (xdotool 'windowmove
+	     window
+	     (- (xt/left extents))
+	     0)))
+
+(define (right-half window)
+  (let ((extents (window-extents window)))
+    (half-screen extents window)
+    (xdotool 'windowmove
+	     window
+	     (+ (/ (root-width) 2)
+		(- (xt/left extents)))
+	     0)))
+
+(define (maximize window)
+  (let ((extents (window-extents window)))
+    (xdotool 'windowmove window 0 0)
+    (xdotool 'windowsize
+	     window
+	     (+ (root-width)
+		(xt/left extents)
+		(xt/right extents))
+	     (+ (root-height)
+		(xt/top extents)
+		(xt/bottom extents)))))
---
+++ adjust-window.sld
@@ -0,0 +1,6 @@
+(define-library (adjust-window)
+  (export active-window half-screen left-half maximize right-half)
+  (import (chibi process))
+  (import (scheme small))
+  (import (srfi 130))
+  (include "adjust-window.scm"))
